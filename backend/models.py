0a7347073d023d0413d50ba159af21503ae0e820
michaelng0107 <33400111+michaelng0107@users.noreply.github.com>
2021-06-05 12:26:15 -0400

    models

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class Test(models.Model):
-    name = models.CharField(max_length=10)
+class Tree(models.Model):
+    species = models.ForiegnKey(Species, related_name='Species', on_delete=models.CASCADE)
+    age = models.PositiveIntegerField()
+    radius = models.PositiveIntegerField()
+    trunk_wounds = models.PositiveIntegerField()
+    mushrooms = models.PositiveIntegerField()
+    surface_roots = models.CharField(max_length=20)
+    leaf_colour = models.CharField(max_length=20)
+
+class Species(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    abundence = models.PositiveIntegerField()
+    profits = models.IntegerField()
+
+
+
     
---
+++ backend/populate.py
@@ -0,0 +1,8 @@
+from .models import Species, Tree
+
+SPECIES = ['AMERICAN BEECH', 'BALD CYPRESS', 'BLACK WILLOW', 'CANADIAN HEMLOCK',
+            'OAK TREE', 'OLIVE TREE', 'PALM TREE', 'RED MAPLE TREE']
+for i in range(11):
+    123
+for i in range(1000):
+    t = Tree()
