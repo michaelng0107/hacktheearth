a9f4221b9edc7ac41409d7e4448c38e126c706d0
michaelng0107 <33400111+michaelng0107@users.noreply.github.com>
2021-06-05 19:15:22 -0400

    added species page

--- backend/urls.py
+++ backend/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('graph2/<int:species>/', views.graph2, name='graph2'),
     path('graph3/<int:species>/', views.graph3, name="graph3"),
     path('graph4/<int:species>/', views.graph4, name='graph4'),
+    path('species/', views.species, name='species'),
 
 ]
